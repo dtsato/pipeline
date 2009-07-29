e98cfa7bd82fb43c058066d118c8ce92dc5e14c6
Danilo Sato <dtsato@gmail.com>
2009-07-29 14:06:57 +0100

    improving documentation

--- examples/two_step_pipeline.rb
+++ examples/two_step_pipeline.rb
@@ -1,19 +1,20 @@
 require File.join(File.dirname(__FILE__), '..', 'init')
-require File.join(File.dirname(__FILE__), '..', 'spec', 'spec_helper')
+require File.join(File.dirname(__FILE__), '..', 'spec', 'database_integration_helper')
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 class Step1 < Pipeline::Stage::Base
   def execute
-    logger.info("Started step 1")
+    puts("Started step 1")
     sleep 2
-    logger.info("Finished step 1")
+    puts("Finished step 1")
   end
 end
 
 class Step2 < Pipeline::Stage::Base
   def execute
-    logger.info("Started step 2")
+    puts("Started step 2")
     sleep 3
-    logger.info("Finished step 2")
+    puts("Finished step 2")
   end
 end
 
